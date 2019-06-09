d83c7d71395f94c4cfe44792a0a82eaf83078a0c
YenR <yen.0001@gmail.com>
2019-06-10 00:24:11 +0900

    Updates

---
+++ precache-manifest.f8e53cd95ebd3908ecdebcd18273a7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277649a218b8cc45d3d73611eab44eb7",
+    "url": "/React-Minesweeper/index.html"
+  },
+  {
+    "revision": "dd7f66ac1b74fedd0909",
+    "url": "/React-Minesweeper/static/css/main.aabf46d8.chunk.css"
+  },
+  {
+    "revision": "81ac5aba8dd5728db825",
+    "url": "/React-Minesweeper/static/js/2.8669071c.chunk.js"
+  },
+  {
+    "revision": "dd7f66ac1b74fedd0909",
+    "url": "/React-Minesweeper/static/js/main.4bd6ddb9.chunk.js"
+  },
+  {
+    "revision": "d875c34d9fc49b4e14b2",
+    "url": "/React-Minesweeper/static/js/runtime~main.8733ca00.js"
+  }
+]);
